158d8efac774b97c4359e0e932611fe84f5e04db
Owen <lebec.owen@yahoo.fr>
2025-04-11 14:23:08 +0200

    config gh pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), sentryVitePlugin({
     org: "jsm-x9",
@@ -11,6 +10,7 @@ export default defineConfig({
     org: "owen-b4",
     project: "javascript-react"
   })],
+  base: "/apple-clone",
 
   build: {
     sourcemap: true
